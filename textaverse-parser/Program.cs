95daa4ee9524c0148c41764a10dcfbcb24727596
Sebastiao Miranda <sebastiao.miranda@outlook.com>
2021-12-26 11:25:03 +0000

    capture elements in lowecase

--- textaverse-parser/Program.cs
+++ textaverse-parser/Program.cs
@@ -34,17 +34,17 @@
     public override Verse VisitCommand([NotNull] TextaverseParser.CommandContext ctx)
     {
 
-      var verbToken = ctx.predicate()?.verb()?.WORD()?.ToString();
-      var adverb = ctx.adverb() != null ? new Adverb(ctx.adverb()?.WORD()?.ToString()) : null;
-      var preposition = ctx.PREPOSITION() != null ? new Preposition(ctx.PREPOSITION()?.ToString()) : null;
+      var verbToken = ctx.predicate()?.verb()?.WORD()?.ToString().ToLowerInvariant();
+      var adverb = ctx.adverb() != null ? new Adverb(ctx.adverb()?.WORD()?.ToString().ToLowerInvariant()) : null;
+      var preposition = ctx.PREPOSITION() != null ? new Preposition(ctx.PREPOSITION()?.ToString().ToLowerInvariant()) : null;
       string quote = ctx.quotedarg()?.ANYWORDQUOTED()?.ToString();
 
-      var indirectObjectToken = ctx.indirectobject() != null ? new IndirectObject(ctx.indirectobject()?.adjectivatedNoun()?.noun()?.WORD().ToString(),
+      var indirectObjectToken = ctx.indirectobject() != null ? new IndirectObject(ctx.indirectobject()?.adjectivatedNoun()?.noun()?.WORD().ToString().ToLowerInvariant(),
                                                                                   null/*ctx.indirectobject()?.adjectivatedNoun()?.adjective()?.WORD().ToString()*/)
                                                              : null;
 
       var directObjects = ctx.@object() != null ? ctx.@object()?.adjectivatedNoun()?
-                                                  .Select(n => new DirectObject(n.noun()?.WORD()?.ToString(),
+                                                  .Select(n => new DirectObject(n.noun()?.WORD()?.ToString().ToLowerInvariant(),
                                                                                 null/*n.adjective()?.WORD()?.ToString()*/ ))
                                                   .ToList()
                                                 : null;
