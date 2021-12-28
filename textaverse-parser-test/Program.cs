65fe8b524e01ec25bde283b8165250530a226f2e
Sebastiao Miranda <sebastiao.miranda@outlook.com>
2021-12-28 21:35:46 +0000

    refactor parser to be a library

---
+++ textaverse-parser-test/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Textaverse.Parser
+{
+  using System;
+  using System.IO;
+  using Textaverse.Models;
+
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      Verse verse = null;
+      var parser = new VerseParser();
+      if (args[0] == "--file")
+      {
+        // dotnet run --file test.vrs
+        verse = parser.Parse(File.ReadAllText(args[1]));
+      }
+      else
+      {
+        // dotnet run 'attack monster with axe, then drink water from well; attack human, then shout ""Death for all humans !"", then drink water.'
+        verse = parser.Parse(args[0]);
+      }
+      Console.WriteLine(verse);
+    }
+  }
+}
