e6aecf8e787fcb1a20886a8c8679d6a8461e1900
Sebastiao Miranda <sebastiao.miranda@outlook.com>
2022-04-29 16:32:41 +0100

    arg to create or not create base world

--- textaverse-silo-admin-client/Program.cs
+++ textaverse-silo-admin-client/Program.cs
@@ -8,6 +8,8 @@ using Orleans.Hosting;
 using Orleans.Streams;
 using System.Threading.Tasks;
 
+var create = args.Length > 0 && args[0] == "--create";
+
 using var client = new ClientBuilder()
                       .UseLocalhostClustering()
                       .AddSimpleMessageStreamProvider("SMSProvider")
@@ -27,38 +29,50 @@ Console.WriteLine();
 
 var room1Id = Guid.Parse("b4d1a6f0-752b-4d3b-a8d4-e878a381b884");
 var room1 = client.GetGrain<IRoomGrain>(room1Id);
-await room1.Cast<IRoomAdministrationGrain>().Configure("Hall", "A simple Hall");
 
-var room2Id = Guid.Parse("f215ead8-eb61-4c38-b403-87c2d938f36b");
-var room2 = client.GetGrain<IRoomGrain>(room2Id);
-await room2.Cast<IRoomAdministrationGrain>().Configure("DarkRoom", "A small dark room");
+if(create) {
+  await room1.Cast<IRoomAdministrationGrain>().Configure("Hall", "A simple Hall");
 
-await room1.Cast<IRoomAdministrationGrain>().AddPassage(
-  new PassagePointer("door",
-                     new GrainPointer(room1.GetPrimaryKey(),
-                                      await room1.GetName(),
-                                      GrainType.Room),
-                     new GrainPointer(room2.GetPrimaryKey(),
-                                      await room2.GetName(),
-                                      GrainType.Room)));
+  var room2Id = Guid.Parse("f215ead8-eb61-4c38-b403-87c2d938f36b");
+  var room2 = client.GetGrain<IRoomGrain>(room2Id);
+  await room2.Cast<IRoomAdministrationGrain>().Configure("DarkRoom", "A small dark room");
 
-var objId = Guid.Parse("e1277f65-7d75-49cf-83e6-b9d206ec2441");
-var obj1 = client.GetGrain<IObjectGrain>(objId);
-await obj1.Configure("orb");
-var op = new ObjectPointer(objId, await obj1.GetName());
-await room1.Cast<IRoomAdministrationGrain>().AddObject(op);
+  await room1.Cast<IRoomAdministrationGrain>().AddPassage(
+    new PassagePointer("door",
+                      new GrainPointer(room1.GetPrimaryKey(),
+                                        await room1.GetName(),
+                                        GrainType.Room),
+                      new GrainPointer(room2.GetPrimaryKey(),
+                                        await room2.GetName(),
+                                        GrainType.Room)));
 
-var objId2 = Guid.Parse("66ee60fc-9f68-4f5b-a8e2-c2c1e7659649");
-var obj2 = client.GetGrain<IForthObjectGrain>(objId2);
-await obj2.Configure("forth-interpreter");
-var op2 = new ObjectPointer(objId2, await obj2.GetName());
-await room1.Cast<IRoomAdministrationGrain>().AddObject(op2);
+  await room2.Cast<IRoomAdministrationGrain>().AddPassage(
+    new PassagePointer("door",
+                      new GrainPointer(room2.GetPrimaryKey(),
+                                        await room2.GetName(),
+                                        GrainType.Room),
+                      new GrainPointer(room1.GetPrimaryKey(),
+                                        await room1.GetName(),
+                                        GrainType.Room)));
 
-var objId3 = Guid.Parse("10a3f1ca-a94b-44b1-a821-9a89faaadc4f");
-var obj3 = client.GetGrain<IForthObjectGrain>(objId3);
-await obj3.Configure("odt-clock");
-var op3 = new ObjectPointer(objId3, await obj3.GetName());
-await room1.Cast<IRoomAdministrationGrain>().AddObject(op3);
+  var objId = Guid.Parse("e1277f65-7d75-49cf-83e6-b9d206ec2441");
+  var obj1 = client.GetGrain<IObjectGrain>(objId);
+  await obj1.Configure("orb");
+  var op = new ObjectPointer(objId, await obj1.GetName());
+  await room1.Cast<IRoomAdministrationGrain>().AddObject(op);
+
+  var objId2 = Guid.Parse("66ee60fc-9f68-4f5b-a8e2-c2c1e7659649");
+  var obj2 = client.GetGrain<IForthObjectGrain>(objId2);
+  await obj2.Configure("forth-interpreter");
+  var op2 = new ObjectPointer(objId2, await obj2.GetName());
+  await room1.Cast<IRoomAdministrationGrain>().AddObject(op2);
+
+  var objId3 = Guid.Parse("10a3f1ca-a94b-44b1-a821-9a89faaadc4f");
+  var obj3 = client.GetGrain<IForthObjectGrain>(objId3);
+  await obj3.Configure("odt-clock");
+  var op3 = new ObjectPointer(objId3, await obj3.GetName());
+  await room1.Cast<IRoomAdministrationGrain>().AddObject(op3);
+}
 
 var playerId = Guid.NewGuid();
 var player = client.GetGrain<IAgentGrain>(playerId);
